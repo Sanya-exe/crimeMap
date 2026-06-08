cfdf9e9579902982dcfefe415e5eb8db020eb916
Sanya <sanyapandey080805@gmail.com>
2026-06-08 11:05:34 +0530

    Fix build.sh - remove unnecessary pip install steps

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 
-
 # Update pip
 pip install --upgrade pip
 
 # Install dependencies
 pip install -r requirements.txt
-
-# Handle specific installation steps for maturin dependencies if needed
-pip install maturin
-
-# Install your Python package
-pip install .
